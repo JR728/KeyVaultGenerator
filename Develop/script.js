26e136f3f30e644a3a6652ecdbe659be6aeb1013
JR728 <135184435+JR728@users.noreply.github.com>
2023-07-02 22:35:11 -0500

    Added confirms on characters

--- Develop/script.js
+++ Develop/script.js
@@ -19,6 +19,19 @@ function generatePassword() {
   while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
     passwordLength = prompt("Please enter a valid password length (between 8 and 128 characters):");
   }
+  includeLowercase = confirm("Include lowercase characters?");
+  includeUppercase = confirm("Include uppercase characters?");
+  includeNumeric = confirm("Include numeric characters?");
+  includeSpecial = confirm("Include special characters?");
+
+  // Ensure at least one character type is selected
+  while (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+    alert("At least one character type must be selected!");
+    includeLowercase = confirm("Include lowercase characters?");
+    includeUppercase = confirm("Include uppercase characters?");
+    includeNumeric = confirm("Include numeric characters?");
+    includeSpecial = confirm("Include special characters?");
+  }
 }
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
